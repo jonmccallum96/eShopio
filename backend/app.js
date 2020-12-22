0ad5acab675a321e7f2267f94f94cd7c1c01fa0d
Jon <jon.a.mccallum@gmail.com>
2020-12-22 13:36:34 +0000

    basic express api setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('api is running');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.listen('5000', console.log('server running on port 5000!'));
