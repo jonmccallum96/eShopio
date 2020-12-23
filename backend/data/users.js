311f33ec4c00fdb65f079733af0bf04027963ad3
Jon <jon.a.mccallum@gmail.com>
2020-12-23 17:47:28 +0000

    add basic bcrypt hashing to mock user passwords

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,22 @@
+import bcrypt from 'bcryptjs';
+
 const users = [
   {
     name: 'Admin User',
     email: 'admin@test.com',
-    password: xxxx,
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
     name: 'Joe Jon',
     email: 'jon@test.com',
-    password: xxxx,
+    password: bcrypt.hashSync('123456', 10),
   },
   {
     name: 'Kelly Clarkson',
     email: 'kelly@test.com',
-    password: xxxx,
+    password: bcrypt.hashSync('123456', 10),
   },
 ];
+
+export default users;
