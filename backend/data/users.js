de3a44c826069a230334fd9275979ec08b4cfb72
Jon <jon.a.mccallum@gmail.com>
2020-12-23 16:47:25 +0000

    create mock user data file

---
+++ backend/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@test.com',
+    password: xxxx,
+    isAdmin: true,
+  },
+  {
+    name: 'Joe Jon',
+    email: 'jon@test.com',
+    password: xxxx,
+  },
+  {
+    name: 'Kelly Clarkson',
+    email: 'kelly@test.com',
+    password: xxxx,
+  },
+];
