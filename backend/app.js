91e8cb8e2bb52eb4cca422b2f89e20566dd23dbe
Jon <jon.a.mccallum@gmail.com>
2020-12-27 11:33:18 +0000

    export custom express middleware to seperate middleware file

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -16,20 +17,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 
-app.use((err, req, res, next) => {
-  const error = new Error(`Not found ${req.originalURL}`);
-  res.status(404);
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  });
-});
+app.use(notFound, errorHandler);
 
 const port = process.env.PORT || 5000;
 
